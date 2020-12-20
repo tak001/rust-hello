ba4606e0d451969c68a423bedf30fc1d153bec09
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-20 17:13:24 +0900

    feat:array

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
     tuple();
+    array();
 }
 
 fn tuple() {
@@ -8,4 +9,12 @@ fn tuple() {
     t.0 = 2;
     t.1 = "3";
     println!("{},{}", t.0, t.1);
+}
+
+fn array() {
+    let mut a: [i32; 3] = [0, 1, 2];
+    let b: [i32; 3] = [0; 3];
+    a[1] = b[1];
+    a[2] = b[2];
+    println!("{:?}", &a[1..3]);
 }
