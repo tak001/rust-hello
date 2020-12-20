b32a79b166d6af05c4eb467c48e446ca5b947bd9
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-20 18:14:55 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+ fn main() {
     println!("Hello, world!");
     tuple();
     array();
@@ -46,4 +46,45 @@ fn foo() {
     let e2 = Event::KeyDown(1);
     let e3 = Event::MouseDown { x: 10, y: 10 };
     println!("{:?} | {:?} | {:?}", e1, e2, e3);
+}
+
+// 標準ライブラリ
+// データが存在しない場合はNone、存在する場合はその型をTとした時
+pub enum Option<T> {
+    None,
+    Some(T),
+}
+
+// Resultは処理の結果が成功か、エラーかを表現できる列挙型
+pub enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+fn res() {
+    let result: Result<i32, String> = Result::Ok(200);
+
+    match result {
+        Result::Ok(code) => println!("code: {}", code),
+        Result::Err(err) => println!("Err: {}", err),
+    }
+}
+
+fn res2() {
+    // Okの場合はOkの中身を展開、Errの場合はunwrap_or(-1)の値を展開
+    // let result: Result<i32, String> = Result::Ok(200);
+    // println!("code: {}", result.unwrap(-1)); // "code: 200"
+    // let result: Result<i32, String> = Result::Err("error".to_string());
+    // println!("code: {}", result.unwrap_or(-1)); // "code: -1"
+}
+
+// and_then はOkだった場合にだけ、指定した関数を実行することができる
+fn func(code: i32) -> Result<i32, String> {
+    println!("code: {}", code);
+    Result::Ok(100)
+}
+
+fn func2() {
+    // let result: Result<i32, String> = Result::Ok(200);
+    // let next_result = result.and_then
 }
